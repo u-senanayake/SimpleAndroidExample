7929160de07eb2784fe2bee3b1f945b194ca63cc
udayanga91 <u.senanayake91@gmail.com>
2015-06-08 08:20:15 +0530

    NewActivity

---
+++ 7 NewActivity/src/com/alumaworks/NewActivity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.alumaworks.NewActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ 7 NewActivity/src/com/alumaworks/NewActivity/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.alumaworks.NewActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Udayanga Senanayake on 6/4/2015.
+ */
+public class SecondActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
